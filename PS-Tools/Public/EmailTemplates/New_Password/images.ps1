ae96e96ecd76143fbe0d4f89e09c740bb5f672f4
Scott1138 <30352284+scott1138@users.noreply.github.com>
2020-06-28 18:53:59 -0500

    documentation and fix for PSScriptAnalyzer warning

--- PS-Tools/Public/EmailTemplates/New_Password/images.ps1
+++ PS-Tools/Public/EmailTemplates/New_Password/images.ps1
@@ -1,3 +1,8 @@
+# This script is called from the Send-Email function to instaniate the $Images variable
+# In the function's scope. The following command tells PSScriptAnalyzer to suppress the rule.
+
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments','')]
+
 $Images = @{
     
 }
